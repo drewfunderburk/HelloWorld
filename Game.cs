c03848b6ad6cb944e070dac9c0c93ef11f58570a
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2020-08-17 16:27:16 -0500

    Added player stats and read name. Printed player health.

--- Game.cs
+++ Game.cs
@@ -8,6 +8,17 @@ namespace HelloWorld
     {
         public void Run()
         {
+            float health = 100.0f;
+            float healthRegen = 20;
+            string name = Console.ReadLine();
+            bool maxLevelReached = false;
+            int maxLevel = 100;
+            int level = 1;
+            bool ready = false;
+
+            Console.WriteLine(health);
+            health = health + healthRegen;
+            Console.WriteLine(name + "just healed " + healthRegen + " health");
 
         }
     }
