4614099ad1307f71ed2bed91270adc44dcd53632
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2020-08-24 15:17:49 -0500

    Added riddle with limited number of tries

--- Game.cs
+++ Game.cs
@@ -9,13 +9,15 @@ namespace HelloWorld
 
             string name, motto;
             float maxHealth = 100;
-            float health = 100;
-            float damage = 10;
+            float playerHealth = 100;
+            float playerDamage = 10;
+            float enemyHealth = 100;
+            float enemyDamage = 5;
             int maxLevel = 100;
             int level = 1;
             string role = "none";
-            bool ready = false;
             bool dead = false;
+
 
             Console.WriteLine("-------------------------------------");
 
@@ -43,20 +45,20 @@ namespace HelloWorld
                 {
                     role = "Knight";
                     maxHealth *= 2;
-                    health = maxHealth;
+                    playerHealth = maxHealth;
 
                     break;
                 }
                 else if (roleInput == '2')
                 {
                     role = "Wizard";
-                    damage *= 2;
+                    playerDamage *= 2;
 
                     break;
                 }
                 else
                 {
-                    Console.WriteLine("\nThat Input is not recognized!");
+                    Console.WriteLine("\nThat Input is not recognized! Try again!");
                 }
             }
 
@@ -65,18 +67,16 @@ namespace HelloWorld
             Console.WriteLine("Name:   " + name);
             Console.WriteLine("Motto:  " + motto);
             Console.WriteLine("Role:   " + role);
-            Console.WriteLine("Health: " + health);
-            Console.WriteLine("Damage: " + damage);
+            Console.WriteLine("Health: " + playerHealth);
+            Console.WriteLine("Damage: " + playerDamage);
             Console.WriteLine("Level:  " + level + "\n\n");
-
-            ready = true;
 
             // Strange beverage
             Console.WriteLine("You've been approached by a mysterious traveler.");
             Console.WriteLine("They offer you a strange beverage");
             Console.WriteLine("Do you accept?");
-            Console.WriteLine(" Press 1 for yes.");
-            Console.WriteLine(" Press 2 for no.");
+            Console.WriteLine("Press 1 for yes.");
+            Console.WriteLine("Press 2 for no.");
 
             while(true)
             {
@@ -84,11 +84,11 @@ namespace HelloWorld
 
                 if (input == '1')
                 {
-                    health = 1;
-                    damage = 500;
+                    playerHealth = 10;
+                    playerDamage = 500;
                     Console.WriteLine("\nYou feel an intense rage boil through you!");
-                    Console.WriteLine("Health: " + health);
-                    Console.WriteLine("Damage: " + damage);
+                    Console.WriteLine("Health: " + playerHealth);
+                    Console.WriteLine("Damage: " + playerDamage);
 
                     break;
                 }
@@ -100,7 +100,83 @@ namespace HelloWorld
                 }
                 else
                 {
-                    Console.WriteLine("\nThat Input is not recognized!");
+                    Console.WriteLine("\nThat Input is not recognized! Try again!");
+                }
+            }
+
+            // Battle
+            while(playerHealth > 0 && enemyHealth > 0)
+            {
+                Console.WriteLine("=============================");
+                Console.WriteLine("It's a fight!\n");
+                Console.WriteLine("Player Health: " + playerHealth);
+                Console.WriteLine("Player Damage: " + playerDamage);
+                Console.WriteLine("Enemy Health:  " + enemyHealth);
+                Console.WriteLine("Enemy Damage:  " + enemyDamage);
+                Console.WriteLine("\n");
+                Console.WriteLine("Press 1 to Attack!");
+
+                while(true)
+                {
+                    char input = Console.ReadKey().KeyChar;
+                    if (input == '1')
+                    {
+                        Console.WriteLine("You deal " + playerDamage + " damage!");
+                        Console.WriteLine("You take " + enemyDamage + " damage!");
+                        playerHealth -= enemyDamage;
+                        enemyHealth -= playerDamage;
+                        if (enemyHealth < 0)
+                            enemyHealth = 0;
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("Press 1 to Attack!");
+                    }
+                }
+
+                if (enemyHealth < 1)
+                {
+                    Console.WriteLine("You win!");
+                    break;
+                }
+                else if (playerHealth < 1)
+                {
+                    Console.WriteLine("You died!");
+                    break;
+                }
+            }
+
+            // Riddle
+            int tries = 5;
+            Console.WriteLine("Another, completely different stranger approaches you and asks you a question...");
+            Console.WriteLine("\nIf all Trimps are Tramps, and all Tramps are Trumps, then all Trimps are deffinitely Trumps. True or false?");
+            Console.WriteLine("Press T for true.");
+            Console.WriteLine("Press F for false.");
+            while(tries >= 0)
+            {
+                Console.WriteLine("You have " + tries + " tries left!");
+                char input = Console.ReadKey().KeyChar;
+                if (input == 't')
+                {
+                    Console.WriteLine("The stranger cackles and holds up a finger.");
+                    Console.WriteLine("You have chosen... wisely.");
+                    Console.WriteLine("With a shambling step, the stranger hobbles away into the distance.");
+                    Console.WriteLine("You feel a rejuvenating power come over you!");
+                    playerHealth = maxHealth;
+                    Console.WriteLine("Player Health: " + playerHealth);
+                    break;
+                }
+                else if (input == 'f')
+                {
+                    Console.WriteLine("The stranger cackles and holds up a finger.");
+                    Console.WriteLine("You have chosen... poorly.");
+                    Console.WriteLine("With a shambling step, the stranger hobbles away into the distance.");
+                    Console.WriteLine("You feel yourself withering away...");
+                    playerHealth *= 0.5f;
+                    Console.WriteLine("Player Health: " + playerHealth);
+                    tries--;
+                    break;
                 }
             }
         }
